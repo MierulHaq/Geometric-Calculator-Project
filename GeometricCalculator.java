93ddf41f4ba3fa23a2a4d04fe821f4c37cb04d4e
MierulHaq <93185057+MierulHaq@users.noreply.github.com>
2021-12-26 23:43:57 +0800

    Update testing
    tambah input

--- GeometricCalculator.java
+++ GeometricCalculator.java
@@ -1,7 +1,7 @@
 public class GeometricCalculator{
   public static void main (String[] args){
-    
+    import java.util.Scanner sc = new java.util.Scanner(System.in);
     System.out.println("Test");
-    
+    int num = sc.nextInt();
   }
 }
